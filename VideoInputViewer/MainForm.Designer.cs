2028a52e04ff6bdd832b6d2562109ebb8c0364db
Simon Watts <13440504+simongwatts@users.noreply.github.com>
2025-04-25 12:49:52 +0100

    Add project files.

---
+++ VideoInputViewer/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace VideoInputViewer
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ComboBox cboCameras;
+        private System.Windows.Forms.ComboBox cboResolutions;
+        private System.Windows.Forms.Button btnSnapshot;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Timer timerFPS;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.cboCameras = new System.Windows.Forms.ComboBox();
+            this.cboResolutions = new System.Windows.Forms.ComboBox();
+            this.btnSnapshot = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.timerFPS = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 48);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 402);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // cboCameras
+            // 
+            this.cboCameras.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cboCameras.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCameras.FormattingEnabled = true;
+            this.cboCameras.Location = new System.Drawing.Point(0, 0);
+            this.cboCameras.Margin = new System.Windows.Forms.Padding(4);
+            this.cboCameras.Name = "cboCameras";
+            this.cboCameras.Size = new System.Drawing.Size(800, 24);
+            this.cboCameras.TabIndex = 1;
+            this.cboCameras.SelectedIndexChanged += new System.EventHandler(this.cboCameras_SelectedIndexChanged);
+            // 
+            // cboResolutions
+            // 
+            this.cboResolutions.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cboResolutions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboResolutions.FormattingEnabled = true;
+            this.cboResolutions.Location = new System.Drawing.Point(0, 24);
+            this.cboResolutions.Margin = new System.Windows.Forms.Padding(4);
+            this.cboResolutions.Name = "cboResolutions";
+            this.cboResolutions.Size = new System.Drawing.Size(800, 24);
+            this.cboResolutions.TabIndex = 2;
+            this.cboResolutions.SelectedIndexChanged += new System.EventHandler(this.cboResolutions_SelectedIndexChanged);
+            // 
+            // btnSnapshot
+            // 
+            this.btnSnapshot.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSnapshot.Location = new System.Drawing.Point(600, 48);
+            this.btnSnapshot.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSnapshot.Name = "btnSnapshot";
+            this.btnSnapshot.Size = new System.Drawing.Size(100, 402);
+            this.btnSnapshot.TabIndex = 3;
+            this.btnSnapshot.Text = "Snapshot";
+            this.btnSnapshot.UseVisualStyleBackColor = true;
+            this.btnSnapshot.Click += new System.EventHandler(this.btnSnapshot_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnRefresh.Location = new System.Drawing.Point(700, 48);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(100, 402);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // timerFPS
+            // 
+            this.timerFPS.Interval = 1000;
+            this.timerFPS.Tick += new System.EventHandler(this.timerFPS_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSnapshot);
+            this.Controls.Add(this.cboResolutions);
+            this.Controls.Add(this.cboCameras);
+            this.Name = "MainForm";
+            this.Text = "Webcam Viewer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ VideoInputViewer/MainForm.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace VideoInputViewer
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private VideoCapabilities[] videoCapabilities;
+        private int framesReceivedCounter;
+        private bool isClosing;
+        private string currentMoniker;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetupForm();
+        }
+
+        private void SetupForm()
+        {
+            TopMost = true;
+            FormBorderStyle = FormBorderStyle.Sizable;
+            DoubleBuffered = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) => InitializeCameraSystem();
+
+        private void InitializeCameraSystem()
+        {
+            RefreshCameraList();
+            timerFPS.Start();
+        }
+
+        private void RefreshCameraList()
+        {
+            cboCameras.BeginUpdate();
+            cboCameras.Items.Clear();
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("No webcams found");
+                return;
+            }
+
+            foreach (FilterInfo device in videoDevices)
+                cboCameras.Items.Add(device.Name);
+
+            cboCameras.EndUpdate();
+            cboCameras.SelectedIndex = 0;
+        }
+
+        private void StartCamera()
+        {
+            if (cboCameras.SelectedIndex < 0 || isClosing) return;
+
+            currentMoniker = videoDevices[cboCameras.SelectedIndex].MonikerString;
+            InitializeVideoSource(currentMoniker);
+            InitializeResolutionControls();
+            StartVideoCapture();
+        }
+
+        private void InitializeVideoSource(string moniker)
+        {
+            StopCamera();
+            videoSource = new VideoCaptureDevice(moniker);
+            videoSource.NewFrame += VideoSource_NewFrame;
+        }
+
+        private void InitializeResolutionControls()
+        {
+            cboResolutions.BeginUpdate();
+            cboResolutions.Items.Clear();
+
+            videoCapabilities = videoSource.VideoCapabilities;
+            if (videoCapabilities != null)
+            {
+                foreach (var capability in videoCapabilities)
+                    cboResolutions.Items.Add($"{capability.FrameSize.Width}x{capability.FrameSize.Height}");
+
+                cboResolutions.SelectedIndex = videoCapabilities.Length - 1;
+            }
+
+            cboResolutions.EndUpdate();
+        }
+
+        private void StartVideoCapture()
+        {
+            if (cboResolutions.SelectedIndex >= 0 && videoCapabilities != null)
+                videoSource.VideoResolution = videoCapabilities[cboResolutions.SelectedIndex];
+
+            videoSource.Start();
+        }
+
+        private void StopCamera()
+        {
+            if (videoSource == null) return;
+
+            if (videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource.NewFrame -= VideoSource_NewFrame;
+            }
+            videoSource = null;
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (isClosing) return;
+
+            try
+            {
+                Interlocked.Increment(ref framesReceivedCounter);
+                UpdateVideoDisplay((Bitmap)eventArgs.Frame.Clone());
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error processing frame: {ex.Message}");
+            }
+        }
+
+        private void UpdateVideoDisplay(Bitmap image)
+        {
+            if (pictureBox.InvokeRequired)
+            {
+                pictureBox.BeginInvoke(new Action(() => SafeUpdatePictureBox(image)));
+            }
+            else
+            {
+                SafeUpdatePictureBox(image);
+            }
+        }
+
+        private void SafeUpdatePictureBox(Bitmap image)
+        {
+            if (isClosing || pictureBox.IsDisposed) return;
+
+            var oldImage = pictureBox.Image;
+            pictureBox.Image = image;
+            oldImage?.Dispose();
+        }
+
+        private void HandleResolutionChange()
+        {
+            if (cboResolutions.SelectedIndex < 0 || string.IsNullOrEmpty(currentMoniker)) return;
+
+            InitializeVideoSource(currentMoniker);
+            StartVideoCapture();
+        }
+
+        private void cboCameras_SelectedIndexChanged(object sender, EventArgs e) => StartCamera();
+
+        private void cboResolutions_SelectedIndexChanged(object sender, EventArgs e) => HandleResolutionChange();
+
+        private void btnSnapshot_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null) return;
+
+            using var saveDialog = new SaveFileDialog
+            {
+                Filter = "JPEG Image|*.jpg|PNG Image|*.png",
+                Title = "Save Snapshot"
+            };
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                var format = Path.GetExtension(saveDialog.FileName).ToLower() == ".png"
+                    ? ImageFormat.Png
+                    : ImageFormat.Jpeg;
+
+                pictureBox.Image.Save(saveDialog.FileName, format);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e) => InitializeCameraSystem();
+
+        private void timerFPS_Tick(object sender, EventArgs e)
+        {
+            var fps = Interlocked.Exchange(ref framesReceivedCounter, 0);
+            Text = $"Webcam Viewer - {(videoSource?.IsRunning == true ? $"{fps} fps" : "Not running")}";
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isClosing = true;
+            StopCamera();
+            timerFPS.Stop();
+
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.Dispose();
+                pictureBox.Image = null;
+            }
+        }
+    }
+}
