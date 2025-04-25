c19245a1006192fca822ef822f21b5ddfbd95f75
Simon Watts <13440504+simongwatts@users.noreply.github.com>
2025-04-25 18:51:22 +0100

    Some changes introduced by Visual Studio designer

--- VideoInputViewer/MainForm.Designer.cs
+++ VideoInputViewer/MainForm.Designer.cs
@@ -55,10 +55,10 @@
             // pictureBox
             // 
             this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox.Location = new System.Drawing.Point(0, 76);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Location = new System.Drawing.Point(0, 92);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(800, 374);
+            this.pictureBox.Size = new System.Drawing.Size(564, 470);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
@@ -68,10 +68,10 @@
             this.cboCameras.Dock = System.Windows.Forms.DockStyle.Top;
             this.cboCameras.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboCameras.FormattingEnabled = true;
-            this.cboCameras.Location = new System.Drawing.Point(0, 28);
-            this.cboCameras.Margin = new System.Windows.Forms.Padding(4);
+            this.cboCameras.Location = new System.Drawing.Point(0, 36);
+            this.cboCameras.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboCameras.Name = "cboCameras";
-            this.cboCameras.Size = new System.Drawing.Size(800, 24);
+            this.cboCameras.Size = new System.Drawing.Size(900, 28);
             this.cboCameras.TabIndex = 1;
             this.cboCameras.SelectedIndexChanged += new System.EventHandler(this.cboCameras_SelectedIndexChanged);
             // 
@@ -80,20 +80,20 @@
             this.cboResolutions.Dock = System.Windows.Forms.DockStyle.Top;
             this.cboResolutions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboResolutions.FormattingEnabled = true;
-            this.cboResolutions.Location = new System.Drawing.Point(0, 52);
-            this.cboResolutions.Margin = new System.Windows.Forms.Padding(4);
+            this.cboResolutions.Location = new System.Drawing.Point(0, 64);
+            this.cboResolutions.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboResolutions.Name = "cboResolutions";
-            this.cboResolutions.Size = new System.Drawing.Size(800, 24);
+            this.cboResolutions.Size = new System.Drawing.Size(900, 28);
             this.cboResolutions.TabIndex = 2;
             this.cboResolutions.SelectedIndexChanged += new System.EventHandler(this.cboResolutions_SelectedIndexChanged);
             // 
             // btnSnapshot
             // 
             this.btnSnapshot.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnSnapshot.Location = new System.Drawing.Point(600, 76);
-            this.btnSnapshot.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSnapshot.Location = new System.Drawing.Point(732, 92);
+            this.btnSnapshot.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSnapshot.Name = "btnSnapshot";
-            this.btnSnapshot.Size = new System.Drawing.Size(100, 374);
+            this.btnSnapshot.Size = new System.Drawing.Size(168, 470);
             this.btnSnapshot.TabIndex = 3;
             this.btnSnapshot.Text = "Snapshot";
             this.btnSnapshot.UseVisualStyleBackColor = true;
@@ -102,10 +102,10 @@
             // btnRefresh
             // 
             this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnRefresh.Location = new System.Drawing.Point(700, 76);
-            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRefresh.Location = new System.Drawing.Point(564, 92);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(100, 374);
+            this.btnRefresh.Size = new System.Drawing.Size(168, 470);
             this.btnRefresh.TabIndex = 4;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
@@ -118,12 +118,14 @@
             // 
             // menuStrip
             // 
+            this.menuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuFile});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(900, 36);
             this.menuStrip.TabIndex = 5;
             this.menuStrip.Text = "menuStrip";
             // 
@@ -138,14 +140,14 @@
             this.toolStripSeparator2,
             this.menuExit});
             this.menuFile.Name = "menuFile";
-            this.menuFile.Size = new System.Drawing.Size(46, 24);
+            this.menuFile.Size = new System.Drawing.Size(54, 32);
             this.menuFile.Text = "&File";
             // 
             // menuRefresh
             // 
             this.menuRefresh.Name = "menuRefresh";
             this.menuRefresh.ShortcutKeys = System.Windows.Forms.Keys.F5;
-            this.menuRefresh.Size = new System.Drawing.Size(224, 26);
+            this.menuRefresh.Size = new System.Drawing.Size(323, 34);
             this.menuRefresh.Text = "&Refresh";
             this.menuRefresh.Click += new System.EventHandler(this.menuRefresh_Click);
             // 
@@ -153,20 +155,20 @@
             // 
             this.menuSnapshot.Name = "menuSnapshot";
             this.menuSnapshot.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.menuSnapshot.Size = new System.Drawing.Size(224, 26);
+            this.menuSnapshot.Size = new System.Drawing.Size(323, 34);
             this.menuSnapshot.Text = "&Snapshot";
             this.menuSnapshot.Click += new System.EventHandler(this.menuSnapshot_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(320, 6);
             // 
             // menuToggleButtons
             // 
             this.menuToggleButtons.Name = "menuToggleButtons";
             this.menuToggleButtons.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
-            this.menuToggleButtons.Size = new System.Drawing.Size(224, 26);
+            this.menuToggleButtons.Size = new System.Drawing.Size(323, 34);
             this.menuToggleButtons.Text = "Hide Buttons";
             this.menuToggleButtons.Click += new System.EventHandler(this.menuToggleButtons_Click);
             // 
@@ -174,28 +176,28 @@
             // 
             this.menuToggleComboboxes.Name = "menuToggleComboboxes";
             this.menuToggleComboboxes.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
-            this.menuToggleComboboxes.Size = new System.Drawing.Size(224, 26);
+            this.menuToggleComboboxes.Size = new System.Drawing.Size(323, 34);
             this.menuToggleComboboxes.Text = "Hide ComboBoxes";
             this.menuToggleComboboxes.Click += new System.EventHandler(this.menuToggleComboboxes_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(221, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(320, 6);
             // 
             // menuExit
             // 
             this.menuExit.Name = "menuExit";
             this.menuExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.menuExit.Size = new System.Drawing.Size(224, 26);
+            this.menuExit.Size = new System.Drawing.Size(323, 34);
             this.menuExit.Text = "E&xit";
             this.menuExit.Click += new System.EventHandler(this.menuExit_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.btnSnapshot);
@@ -203,10 +205,11 @@
             this.Controls.Add(this.cboCameras);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "Webcam Viewer";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
